7a24124ac2f5171599adbd773065b865ef384074
Ferolandm <gameferoland@gmail.com>
2025-11-18 14:31:20 +0700

    .

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -6,19 +6,29 @@ use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
 {
-    /**  
+    /**
      * Seed the application's database.
-     *
-     * @return void
      */
     public function run()
     {
+        // Matikan FK
         DB::statement('SET FOREIGN_KEY_CHECKS=0');
 
+        // Opsional: truncate tabel yang akan diisi
         DB::table('users')->truncate();
+        DB::table('distributors')->truncate(); // jika ada
+        DB::table('menus')->truncate();
+        DB::table('user_access')->truncate();
 
-        $this->call([ UsersTableSeeder::class]);
+        // Jalankan semua seeder
+        $this->call([
+            UsersTableSeeder::class,
+            DistributorTableSeeder::class,
+            MenusTableSeeder::class,
+            UserAccessTableSeeder::class,
+        ]);
 
+        // Nyalakan kembali FK
         DB::statement('SET FOREIGN_KEY_CHECKS=1');
     }
 }
